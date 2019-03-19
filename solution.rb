2c95c4057300da05c8bf23800bcf93c6ce3c136f
sebastian argaez <sargaezu@gmail.com>
2019-03-19 17:28:26 -0500

    intento5

--- solution.rb
+++ solution.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 get "/" do
     if params[:nombre].nil? || params[:nombre].empty?
-   "<h1>Hola Desconocido</h1>"
+   "<h1>Hola desconocido!</h1>"
      else params[:nombre]
    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
